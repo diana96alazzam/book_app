ad33419996425ef85c9d9fb32603c449d7834834
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-04-12 11:48:57 +0300

    initial steps

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(express.static('./public/styles'))
+
+app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-    response.status(200).send('test main page');
+    //data = data from api I think or sql
+    response.render('index.ejs', data );
 });
+
+app.get('/hello', (request, response) => {
+    response.render('pages/index' );
+});
+
 
 app.use('*', (request, response) => {
     response.status(404).send('Page not found');
