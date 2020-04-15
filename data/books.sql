e7a38ca8828e9e6a507ea83901c7a11a590e76b2
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-04-15 03:43:47 +0300

    tried working on shelves

--- data/books.sql
+++ data/books.sql
@@ -8,4 +8,11 @@ CREATE TABLE books(
     image_url VARCHAR(65535),
     description TEXT,
     bookshelf VARCHAR(255)
+);
+
+CREATE TABLE bookshelvesTable(
+    id SERIAL PRIMARY KEY,
+    bookshelfName VARCHAR(255)
 )
+
+
